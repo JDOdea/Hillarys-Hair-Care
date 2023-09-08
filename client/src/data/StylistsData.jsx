746f71c14d7254e6242cf20431991d298f71d9f5
JDOdea <jondavidodea@gmail.com>
2023-09-08 11:43:47 -0500

    implemented adding stylists

--- client/src/data/StylistsData.jsx
+++ client/src/data/StylistsData.jsx
@@ -4,3 +4,12 @@ const _apiUrl = "/api/stylists";
 export const getStylists = async () => {
     return await fetch(_apiUrl).then((r) => r.json());
 }
+
+//  Add Stylist
+export const createStylist = (stylist) => {
+    return fetch(_apiUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(stylist),
+    }).then((res) => res.json());
+}
